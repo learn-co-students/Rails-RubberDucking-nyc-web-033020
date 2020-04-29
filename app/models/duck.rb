4155d539b6e3d8cae14e661d44a83365b2223816
github username <github email address>
2020-04-29 10:28:56 -0400

    commit

--- app/models/duck.rb
+++ app/models/duck.rb
@@ -1,2 +1,11 @@
 class Duck < ApplicationRecord
+    belongs_to :student 
+
+    def student_name=(name) 
+        self.student = Student.find_or_create_by(name: name) 
+    end 
+
+    def student_name 
+        self.student ? self.student.name : nil 
+    end 
 end
