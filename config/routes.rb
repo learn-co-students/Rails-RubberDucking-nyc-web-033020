e67436350b7a7d547e79499dc377d0b321fc3e78
github username <github email address>
2020-04-29 10:57:43 -0400

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'students/new'
+  get 'students/create'
+  get 'students/edit'
+  get 'students/update'
+  get 'students/destroy'
+  get 'students/show'
   resources :ducks
   resources :students
 end
